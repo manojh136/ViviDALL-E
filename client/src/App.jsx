a9dd5338253509d3a8c4b204e18386b90cc82ebb
manojh136 <58912761+manojh136@users.noreply.github.com>
2023-08-04 17:48:56 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/" >
+        <h1 className='font-extrabold text=[#222328] text-[28px]'><span class='italic'>Vivi</span>DALL-E</h1>
+        <p className='flex justify-center text-xs'> &nbsp;powered by&nbsp;
+        <img src={logo} alt="logo" className="object-scale-down h-15 w-20  " />
+        </p>
+      </Link>
+
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
